051f267c97dd3ed7856197971b351c585c0736b9
Shapeshifter73 <phillip.a.jordan@gmail.com>
2017-02-13 10:55:46 +0700

    added quotes for phone number in profile page

--- profile/index.php
+++ profile/index.php
@@ -13,7 +13,7 @@ header("Location: ../login");
 <h2 id="header">Profile</h2>
 <form id="profile" action="changeprofile.php" method="POST">
 Email: <input type="email" value=<?php echo "'".$_SESSION['email']."'"; ?> /><br/>
-Phone: <input type="tel" value=<?php echo $_SESSION['phone']; ?> /><br/>
+Phone: <input type="tel" value='<?php echo $_SESSION["phone"]; ?>' /><br/>
 Credit Card: <input type="number" value=<?php echo "'".$_SESSION['credit']."'"; ?> /><br/>
 </form>
 </body>
