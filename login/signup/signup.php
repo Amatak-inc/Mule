39c26ef43826720392b01081518badda94af7163
Shapeshifter73 <phillip.a.jordan@gmail.com>
2017-02-13 17:52:17 +0700

    debugging signup error 5

--- login/signup/signup.php
+++ login/signup/signup.php
@@ -5,16 +5,14 @@ $user = "root";
 $password = "973610";
 $port = 8889;
 $db = "Mule";
-$_SESSION["signuperror"] = null;
 if ($conn->connect_error) {
     die("not working 3");
-    die();
 }
 $conn = new mysqli($host, $user, $password, $db) or die("not working 2");
 $query2 = mysqli_query($conn, "SELECT * FROM users where email = '".$_POST["email"]."';") or die ("test1");
 $query3 = mysqli_query($conn, "SELECT * FROM users where credit = '".$_POST["credit"]."';") or die ("test2");
 $query4 = mysqli_query($conn, "SELECT * FROM users where phone = '".$_POST["phone"]."';") or die ("test3");
-echo($query2);
+echo($query2) or die("test123");
 /*if ($_POST["pw"] !== $_POST["repassword"]){
     $_SESSION["signuperror"] = "passwords are different";
     header("Location: .");
