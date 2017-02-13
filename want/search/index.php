161400656d283abded6d499e9ff578125a63cb11
Shapeshifter73 <phillip.a.jordan@gmail.com>
2017-02-13 20:03:28 +0700

    put template elements in search page

--- want/search/index.php
+++ want/search/index.php
@@ -10,3 +10,144 @@
 
     }
 ?>
+<!doctype html>
+<head>
+<title>Mule | search</title>
+</head>
+<body align="center">
+
+<div id="title" align="left"><h2>Mule</h2></div>
+
+<div id="topbar">
+
+    <ul id="toplist">
+
+        <a href="../" class="black"><li class="topbarlist">Home</li></a>
+
+        <a href="../going" class="black"><li class="topbarlist">Going Somewhere</li></a>
+
+        <li class="topbarlist">Need Something</li>
+
+    </ul>
+
+</div>
+
+<div id="container">
+
+    
+
+</div>
+
+</body>
+</html>
+<style type="text/css">
+#container {
+
+    position: relative;
+
+    left: 35%;
+
+    border: 1px solid black;
+
+    border-radius: 5px;
+
+    width: 25%;
+
+    font-family: sans-serif;
+
+    background-color: #D8D8D8;
+
+    margin-top: 15%;
+
+    padding: 20px;
+
+}
+
+button {
+
+    background-color: #01DF01;
+
+    border: 1px solid black;
+
+    border-radius: 5px;
+
+    margin: 5px;
+
+}
+
+#title {
+
+    background: white;
+
+    margin: 0;
+
+}
+
+#topbar {
+
+    background-color: #2ECCFA;
+
+    padding: 10px;
+
+}
+
+li.topbarlist {
+
+    border-left: 1px black solid;
+
+    padding: 10px;
+
+    display: inline;
+
+}
+
+.black {
+
+    color: black;
+
+    text-decoration: none;
+
+}
+
+input {
+
+    border: 1px solid #151515;
+
+}
+
+li.topbarlist:hover {
+
+    background-color: #01A9DB;
+
+}
+
+#current {
+
+    background-color: #3ADF00;
+
+}
+
+#toplist {
+
+    list-style-type: none;
+
+    margin: 0;
+
+    padding: 0;
+
+}
+
+body {
+
+    margin: 0;
+
+    background-image: url("../images/hong kong.jpg");
+
+}
+
+h2 {
+
+    margin: 1px;
+
+}
+</style>
