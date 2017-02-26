ce0a36e4dfa9f7dd20981e03dc091579a458fc68
Shapeshifter73 <phillip.a.jordan@gmail.com>
2017-02-26 09:37:15 +0700

    added index.php

--- profile/index.php
+++ profile/index.php
@@ -14,7 +14,19 @@ header("Location: ../login");
 <form id="profile" action="changeprofile.php" method="POST">
 Email: <input type="email" value=<?php echo "'".$_SESSION['email']."'"; ?> /><br/>
 Phone: <input type="tel" value='<?php echo $_SESSION["phone"]; ?>' /><br/>
-Credit Card: <input type="number" value=<?php echo "'".$_SESSION['credit']."'"; ?> /><br/>
+Credit Card: <input type="number" value=<?php echo "'".$_SESSION['credit']."'"; ?> /><br/><br/>
+<div id="travels">
+<h2>travels</h2>
+<div id="travellist">
+<h3>planned</h3>
+
+<h3>confirmed</h3>
+
+<h3>ongoing</h3>
+
+<h3>finished</h3>
+</div>
+</div>
 </form>
 </body>
 </html>
